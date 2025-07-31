91c47951a8c6f57eae979928613dd709299320fb
S VIVEGANANDAN <111070769+Vickydmt@users.noreply.github.com>
2025-07-31 13:13:41 +0530

    Update advanced-ocr.ts

--- lib/advanced-ocr.ts
+++ lib/advanced-ocr.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const GOOGLE_VISION_API_KEY = "AIzaSyA2xoFUwlBNOK112_DwXJy2Pq5Tqlp_7PA";
+const GOOGLE_VISION_API_KEY = "AIzaSyCLVG91jIazGPPi5GEpxiwWqSqXP4MhmFc";
 const GOOGLE_VISION_ENDPOINT = `https://vision.googleapis.com/v1/images:annotate?key=${GOOGLE_VISION_API_KEY}`;
 
 export async function processHistoricalDocument(file: File): Promise<string> {
